8fd9138970de7cb0744f7ea2c33b7e09839a88f0
hippo420 <hippo420@naver.com>
2025-10-18 16:43:29 +0900

    수정

--- src/test/java/app/movida/MovidaApplicationTests.java
+++ src/test/java/app/movida/MovidaApplicationTests.java
@@ -1,9 +1,12 @@
 package app.movida;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.config.ConfigServerAutoConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {ConfigServerAutoConfiguration.class})
 class MovidaApplicationTests {
 
     @Test
